ff406a1fb6ead38791fd36b38822fcfd100ee4cf
JaeHo Song <thruthesky@gmail.com>
2018-08-12 16:51:11 +0800

    update for login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -5,8 +5,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  templateUrl: './login.page.html'
 })
 export class LoginPage implements OnInit {
 
