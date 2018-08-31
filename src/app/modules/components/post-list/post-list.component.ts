3a608a8dfc06620033259e9158fbea65e8238676
JAEHO SONG <thruthesky@gmail.com>
2018-08-31 23:26:42 +0800

    update up

--- src/app/modules/components/post-list/post-list.component.ts
+++ src/app/modules/components/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PhilGoApiService } from '../../philgo-api/philgo-api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-list',
@@ -9,10 +10,20 @@ import { PhilGoApiService } from '../../philgo-api/philgo-api.service';
 export class PostListComponent implements OnInit {
 
   constructor(
+    private activatedRoute: ActivatedRoute,
     public philgo: PhilGoApiService
   ) {
-    philgo.forumPage({ post_id: 'freetalk', page_no: 1, limit: 10 }).subscribe(res => {
-      console.log('res: ', res);
+    // philgo.forumPage({ post_id: 'freetalk', page_no: 1, limit: 10 }).subscribe(res => {
+    //   console.log('res: ', res);
+    // });
+
+    activatedRoute.paramMap.subscribe(params => {
+      console.log('params: ', params);
+
+      philgo.postSearch({ post_id: params.get('post_id') }).subscribe(search => {
+        console.log('serach', search);
+      })
+
     });
   }
 
