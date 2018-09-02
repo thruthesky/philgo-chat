37c753cc2bc56fcacfac81dbcc741bad3c00a782
JAEHO SONG <thruthesky@gmail.com>
2018-09-02 17:37:03 +0800

    update for camera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Router } from '../../node_modules/@angular/router';
 import { AppService } from './providers/app.service';
 import { LanguageTranslate } from './modules/language-translate/language-translate';
 import { PhilGoApiService } from './modules/philgo-api/philgo-api.service';
-import { PhilGoApiTestService } from './modules/philgo-api/philgo-api.test';
+
 
 
 @Component({
