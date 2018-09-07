66fb9ab9ad26d1dae478a4ea80da7030c787d566
JAEHO SONG <thruthesky@gmail.com>
2018-09-07 20:11:24 +0800

    update for main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'my-rooms' },
+  { path: '', pathMatch: 'full', loadChildren: './pages/my-rooms/my-rooms.module#MyRoomsPageModule' },
+  { path: 'my-rooms', loadChildren: './pages/my-rooms/my-rooms.module#MyRoomsPageModule' },
+  // { path: '', pathMatch: 'full', redirectTo: 'my-rooms' },
   // { path: '', pathMatch: 'full', loadChildren: './pages/home/home.module#HomePageModule' },
   { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
   { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
@@ -13,7 +15,6 @@ const routes: Routes = [
   { path: 'create-room', loadChildren: './pages/create-room/create-room.module#CreateRoomPageModule' },
   { path: 'help', loadChildren: './pages/help/help.module#HelpPageModule' },
   { path: 'setting', loadChildren: './pages/setting/setting.module#SettingPageModule' },
-  { path: 'my-rooms', loadChildren: './pages/my-rooms/my-rooms.module#MyRoomsPageModule' },
   { path: 'all-rooms', loadChildren: './pages/all-rooms/all-rooms.module#AllRoomsPageModule' },
   { path: 'setting-dev-info', loadChildren: './pages/setting-dev-info/setting-dev-info.module#SettingDevInfoPageModule' },
   { path: 'setting-language', loadChildren: './pages/setting-language/setting-language.module#SettingLanguagePageModule' },
