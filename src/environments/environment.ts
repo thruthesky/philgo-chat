3704167f56a96952ac63e0af91cf0f2601cbe037
JAEHO SONG <thruthesky@gmail.com>
2018-09-13 17:38:01 +0800

    Update for env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,17 +5,17 @@
 export const environment = {
   production: false,
   hmr: true,
-  philgoServerUrl: 'https://www.philgo.com/api.php',
-  philgoFileServerUrl: 'https://file.philgo.com/index.php',
-  newFileServerUrl: 'https://file.philgo.com/~file_server/index.php'
+  // philgoServerUrl: 'https://www.philgo.com/api.php',
+  // philgoFileServerUrl: 'https://file.philgo.com/index.php',
+  // newFileServerUrl: 'https://file.philgo.com/~file_server/index.php'
 
   // philgoServerUrl: 'https://local.philgo.com/api.php',
   // philgoFileServerUrl: 'https://local.philgo.com/index.php',
   // newFileServerUrl: 'http://work.org/file-server/index.php',
 
-  // philgoServerUrl: 'http://192.168.0.254/sapcms_1_2/api.php',
-  // philgoFileServerUrl: 'http://192.168.0.254/sapcms_1_2/index.php',
-  // newFileServerUrl: 'http://192.168.0.254/file-server/index.php',
+  philgoServerUrl: 'http://192.168.0.254/sapcms_1_2/api.php',
+  philgoFileServerUrl: 'http://192.168.0.254/sapcms_1_2/index.php',
+  newFileServerUrl: 'http://192.168.0.254/file-server/index.php',
 
   /**
    * Office network.
